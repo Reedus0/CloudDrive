e592b6aaadd014c26b2fa7bd44327cdc72f0ee22
Reedus0 <lazsasha2006@gmail.com>
2022-12-10 17:24:59 +0300

    added icon for theme change button, changed colors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,7 +24,9 @@ const Header: FC = () => {
                 <h1 className='profile-header__name'>{window.innerWidth < 767 ? user['username'] : 'С возвращением, ' + user['username'] + '!'}</h1>
               </div>
               <div className='header__right right-header'>
-              <button className='header__theme-button' onClick={() => setTheme(theme === IThemes.LIGHT ? IThemes.DARK : IThemes.LIGHT)}>Theme change</button>
+                <button className='header__theme-button' onClick={() => setTheme(theme === IThemes.LIGHT ? IThemes.DARK : IThemes.LIGHT)}>
+                  <img className='header__theme-button-img' width={40} height={40} src='https://github.com/Reedus0/CloudDrive/blob/CloudDrive-master/src/img/theme.png?raw=true' />
+                </button>
                 <Button name="Выйти" function={() => logout()} />
               </div>
             </>
@@ -33,7 +35,9 @@ const Header: FC = () => {
               <div className='profile-header__user'>
               </div>
               <div className='header__right right-header'>
-              <button className='header__theme-button' onClick={() => setTheme(theme === IThemes.LIGHT ? IThemes.DARK : IThemes.LIGHT)}>Theme change</button>
+                <button className='header__theme-button' onClick={() => setTheme(theme === IThemes.LIGHT ? IThemes.DARK : IThemes.LIGHT)}>
+                  <img className='header__theme-button-img' width={40} height={40} src='https://github.com/Reedus0/CloudDrive/blob/CloudDrive-master/src/img/theme.png?raw=true' />
+                </button>
                 <Button name="Войти" function={() => setPrompt(
                   <Prompt title="Вход">
                     <FormLogin />
